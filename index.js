194b198df2d985dfb756d2e04d492b48b805bfd1
Ian Fletcher <ianfletcher314@gmail.com>
2021-04-08 16:47:51 -0500

    this commit has js files made for tests and for command line app. currently working on command line app but cant get it to loop

---
+++ index.js
@@ -0,0 +1,145 @@
+// make command line function that asks the user information about the staff
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown');
+
+const mngrQuestions = [
+    {
+        type: 'input',
+        message: 'What is the team managers name?',
+        name: 'mngrName',
+    },
+    {
+        type: 'input',
+        message: 'What is the managers employee ID #?',
+        name: 'mngrID',
+    },
+    {
+        type: 'input',
+        message: 'Please enter managers email address!',
+        name: 'mngrEmail',
+    },
+    {
+        type: 'input',
+        message: 'Please enter managers office #!',
+        name: 'mngrOfficeNumber',
+    },
+    {
+        type: "list",
+        message: "If there are more staff members please add them!",
+        name: "choiceEnder",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Finish Building Team", 
+        ]
+    },
+];
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        message: 'What is the engineers name?',
+        name: 'engineerName',
+    },
+    {
+        type: 'input',
+        message: 'What is the engineers employee ID #?',
+        name: 'engineerID',
+    },
+    {
+        type: 'input',
+        message: 'Please enter engineers email address!',
+        name: 'engineerEmail',
+    },
+    {
+        type: 'input',
+        message: 'Please enter engineers GitHub user name!',
+        name: 'engineerGitHub',
+    },
+    {
+        type: "list",
+        message: "If there are more staff members please add them!",
+        name: "choiceEnder",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Finish Building Team",
+        ]
+    },
+];
+
+const internQuestions = [
+    {
+        type: 'input',
+        message: 'What is the interns name?',
+        name: 'internName',
+    },
+    {
+        type: 'input',
+        message: 'What is the interns employee ID #?',
+        name: 'internID',
+    },
+    {
+        type: 'input',
+        message: 'Please enter interns email address!',
+        name: 'internEmail',
+    },
+    {
+        type: 'input',
+        message: 'Please enter interns affiliated school!',
+        name: 'internSchool',
+    },
+    {
+        type: "list",
+        message: "If there are more staff members please add them!",
+        name: "choiceEnder",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Finish building my team",
+        ]
+    },
+];
+
+// TODO: Create a function to initialize app 
+function init() {
+    inquirer
+        .prompt(mngrQuestions)
+        .then((data) => {
+            // looping function that calls engineer and intern questions if applicable
+            function looper() {
+            if (data.choiceEnder == 'Engineer'){
+                inquirer
+                    .prompt(engineerQuestions)
+                console.log(data.choiceEnder)
+                
+            }
+            else if (data.choiceEnder == 'Intern'){
+                inquirer
+                    .prompt(internQuestions)
+                console.log(data.choiceEnder)
+                
+            }
+            
+        }
+            // calling looping function 
+            if (data.choiceEnder != "Finish building my team"){
+                console.log("it loooooped")
+                looper()
+            }
+            
+            console.log(data.choiceEnder)
+            // this is taking the data we get and sending it into generateMarkdown.js
+            var markdown = generateMarkdown(data)
+            // this is getting the markdwon from generateMarkdwon and writting a staff.html file 
+            fs.writeFile(`staff.html`, markdown, (err) =>
+              err ? console.log(err) : console.log('Success!')
+            );
+          });
+            
+    
+    }
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,11 @@
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  const title = `${data.mngrName}'s Staff`;
+    return(`${title} is kidna working`
+    // this is where the HTML you write will go 
+);
+ 
+}
+// exports this file out to index.js so it  can be used as a module
+module.exports = generateMarkdown;
